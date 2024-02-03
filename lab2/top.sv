8f8b1a5a52c4e2ff54191a6bfa3b716446c1e6cd
bhendi-boi <bhendiisgreen@gmail.com>
2024-02-03 20:48:12 +0530

    added a new task `display_results` and formatted the file

--- lab2/top.sv
+++ lab2/top.sv
@@ -2,7 +2,8 @@
 
 module top ();
 
-	integer SEED = 0;
+  integer SEED = 0;
+  integer errors = 0;
   reg reset;
 
   reg txclk;
@@ -66,78 +67,85 @@ module top ();
   );
 
   initial begin
-    #32
-    // checking transmission
-    rx_enable = 1;
-    transmission_seq();
-    check_reciever();#32
+    #32 rx_enable = 1;
     transmission_seq();
     check_reciever();
+    #32 transmission_seq();
+    check_reciever();
+
+    display_results();
   end
 
   always @(posedge rxclk) begin
-    if(rx_enable)
-      rx_in = tx_out;
+    if (rx_enable) rx_in = tx_out;
   end
 
   always @(posedge rxclk) begin
-    if(rx_enable && !rx_empty) begin
-      uld_rx_data = 1; #10 uld_rx_data = 0;
+    if (rx_enable && !rx_empty) begin
+      uld_rx_data = 1;
+      #10 uld_rx_data = 0;
     end
   end
 
-  task transmission_seq ();
+  task transmission_seq();
     tx_enable = 1;
-    randomise_data(); 
-    ld_tx_data = 1; #32
-    ld_tx_data = 0;
+    randomise_data();
+    ld_tx_data = 1;
+    #32 ld_tx_data = 0;
     check_transmission();
     tx_enable = 0;
   endtask
 
-  task randomise_data ();
+  task randomise_data();
     tx_data = $random(SEED);
-		$display("[Randomise Data] Random Data Produced is : 8'h%h",tx_data);
+    $display("[Randomise Data] Random Data Produced is : 8'h%h", tx_data);
   endtask
 
-  task check_transmission ();
+  task check_transmission();
     tx_out_check = 9'h0;
     for (int i = 0; i < 10; i++) begin
       @(posedge txclk) begin
-        #1
-        tx_out_check[i] = tx_out;
+        #1 tx_out_check[i] = tx_out;
       end
     end
 
     transmitted_data = tx_out_check[8:1];
-    if(tx_out_check[0] == 0 && tx_out_check[9] == 1 && transmitted_data == tx_data) begin 
-      $display("[Check Transmission] Transimssion Successful. Transmitted data: %h",transmitted_data);
-    end
-    else begin
+    if (tx_out_check[0] == 0 && tx_out_check[9] == 1 && transmitted_data == tx_data) begin
+      $display("[Check Transmission] Transimssion Successful. Transmitted data: %h",
+               transmitted_data);
+    end else begin
+      errors = errors + 1;
       $display("[Check Transmission] Transmission Failed.");
-      if(tx_out_check[0] != 0) begin
+      if (tx_out_check[0] != 0) begin
         $display("Start bit is missing.\n");
-      end
-      else if (tx_out_check[9] != 1) begin
+      end else if (tx_out_check[9] != 1) begin
         $display("End bit is missing.\n");
-      end
-      else begin
-        $display("Problem with transmitted data. Data transmitted is %h\n",transmitted_data);
+      end else begin
+        $display("Problem with transmitted data. Data transmitted is %h\n", transmitted_data);
       end
     end
   endtask
 
-  task check_reciever ();
+  task check_reciever();
     @(posedge uld_rx_data) begin
       @(posedge rxclk) begin
         recieved_data = rx_data;
-        if(transmitted_data == recieved_data) begin
-          $display("[Check Reciever] Data recieved sucessfully. Recieved data: 8'h%h\n",recieved_data);
-        end
-        else begin
-          $display("[Check Reciever] Corrupt data recieved. Recieved data: 8'h%h\n",recieved_data);
+        if (transmitted_data == recieved_data) begin
+          $display("[Check Reciever] Data recieved sucessfully. Recieved data: 8'h%h\n",
+                   recieved_data);
+        end else begin
+          errors = errors + 1;
+          $display("[Check Reciever] Corrupt data recieved. Recieved data: 8'h%h\n", recieved_data);
         end
       end
+    end
+  endtask
+
+  task display_results();
+    if (!errors) begin
+      $display("\t\t#### TEST PASSED ####");
+    end else begin
+      $display("\t\t#### TEST FAILED ####`");
     end
   endtask
 
